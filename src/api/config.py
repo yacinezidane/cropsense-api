f9fd96b75f79238474485cc0a8f1c9873a1f6036
yacinezidane <142247720+yacinezidane@users.noreply.github.com>
2026-06-08 22:23:54 +0100

    Update BASE_DIR to use relative path

--- src/api/config.py
+++ src/api/config.py
@@ -15,7 +15,7 @@ MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "plant_disease_db")
 # ─────────────────────────────
 # 🧠 AI Model (TFLite LOCAL)
 # ─────────────────────────────
-BASE_DIR = Path("/opt/render/project")
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 MODEL_DIR = BASE_DIR / "models"
 
