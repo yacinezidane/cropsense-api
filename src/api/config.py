bcab0823eafa4a35f795696ccd8b61244ccb3529
yacinezidane <142247720+yacinezidane@users.noreply.github.com>
2026-06-08 22:58:16 +0100

    Change BASE_DIR to '/opt/render/project/src'
    Update BASE_DIR to a specific path for deployment.

--- src/api/config.py
+++ src/api/config.py
@@ -15,7 +15,7 @@ MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "plant_disease_db")
 # ─────────────────────────────
 # 🧠 AI Model (TFLite LOCAL)
 # ─────────────────────────────
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path("/opt/render/project/src")   # ✅ صحيح
 
 
 MODEL_DIR = BASE_DIR / "models"
